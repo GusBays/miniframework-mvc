156d5aba73141f48bba430993d26a356497444d6
Gustavo Bays <gustavo_boliv@hotmail.com>
2022-02-24 21:17:17 -0300

    feat: added dinamic views

--- App/Controllers/IndexController.php
+++ App/Controllers/IndexController.php
@@ -2,14 +2,38 @@
 
 namespace App\Controllers;
 
+use stdClass;
+
 class IndexController {
 
+    private $view;
+
+    public function __construct() {
+        //cria um objeto vazio usando a classe standart do php que pode ser usado nos demais métodos e recuperado nas views
+        $this->view = new \stdClass();
+    }
+
     public function index() {
-        echo 'chegamos ao index controller e disparou index';
+        $this->render('index');
     }
 
     public function sobreNos() {
-        echo 'chegamos ao index controller e disparou sobreNos';
+        $this->render('sobreNos');
+    }
+
+    public function render($view) {
+
+        //pega o nome da classe atual
+        $classAtual = get_class($this);
+
+        //retira o nome dos diretórios porque todos os controllers ficam na mesma pasta
+        $classAtual = str_replace('App\\Controllers\\', '', $classAtual);
+
+        //remove a palavra controller do nome do arquivo, pra pegar apenas o nome do controlador e usar dinamicamente no require
+        $classAtual = strtolower(str_replace('Controller', '', $classAtual));
+
+        //o caminho dos requires sempre irá partir do arquivo index.php, dentro da public por conta do autoload do composer
+        require_once "../App/Views/".$classAtual."/".$view.".phtml";
     }
 }
 
